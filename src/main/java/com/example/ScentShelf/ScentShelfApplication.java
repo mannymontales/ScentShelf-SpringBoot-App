d6ef06b4f6eb81940df759a73102100abbb50d46
Manny <manny@Zipcoders-MacBook-Pro-58.local>
2023-08-21 16:28:03 -0400

    initial commit

---
+++ src/main/java/com/example/ScentShelf/ScentShelfApplication.java
@@ -0,0 +1,13 @@
+package com.example.ScentShelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScentShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ScentShelf/ScentShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ScentShelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
